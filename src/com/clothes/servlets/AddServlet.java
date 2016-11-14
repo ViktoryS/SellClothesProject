0071ad4363848d96d94d03c16342980a1d5c1d2e
ViktoryS <viviktorys@gmail.com>
2016-11-14 11:22:28 +0200

    14.11 commit

--- src/com/clothes/servlets/AddServlet.java
+++ src/com/clothes/servlets/AddServlet.java
@@ -2,6 +2,7 @@ package com.clothes.servlets;
 
 import com.clothes.dao.ClothesStorage;
 import com.clothes.model.ClothBuilder;
+import com.clothes.utils.UtilCloth;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,11 +23,7 @@ public class AddServlet extends HttpServlet {
 		String rqSize = request.getParameter("size");
 		String rqPrice = request.getParameter("price");
 
-		//TODO: Remove comments
-		//responceWriter.print("name = |" + rqName + "|        size = |" + rqSize + "|      price = |" + rqPrice + "|");
-		
-		//TODO: Create Util class for null value verification
-		if (rqName == null || rqName.equals("") || rqSize == null || rqSize.equals("") || rqPrice == null || rqPrice.equals("")){
+		if (UtilCloth.ParamsVerification(rqName, rqPrice, rqSize)){
 			responceWriter.print("<h1> Error with parameters:&nbsp name = " + rqName +
 					", size = " + rqSize + ", price = " + rqPrice + "</h1>");
 		}else {
@@ -35,14 +32,14 @@ public class AddServlet extends HttpServlet {
 			Double price = null;
 			try {
 				price = Double.parseDouble(rqPrice);
+				ClothesStorage.addCloth(new ClothBuilder().buildName(name).
+						buildSize(size).buildPrice(price).buildCloth());
+				responceWriter.print("<h1> Congradulations! Cloth " + name + " was added successfully! </h1>");
+
 			}catch (NumberFormatException e){
 				responceWriter.print("<h1>The price must be a number! Please, input number.</h1>");
 			}
-			//TODO: Fix this bug
-			ClothesStorage.addCloth(new ClothBuilder().buildName(name).
-					buildSize(size).buildPrice(price).buildCloth());
 
-			responceWriter.print("<h1> Congradulations! Cloth " + name + " was added successfully! </h1>");
 		}
 		responceWriter.close();
 	}
---
+++ src/com/clothes/utils/UtilCloth.java
@@ -0,0 +1,13 @@
+package com.clothes.utils;
+
+public class UtilCloth {
+
+    public static boolean ParamsVerification(String... params){
+        for (String parameter: params){
+            if(parameter == null || parameter.equals(""))
+                return false;
+        }
+        return true;
+    }
+
+}
